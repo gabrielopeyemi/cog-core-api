74cb051b2390efed7dd30c439e06711fc4ecb8d3
gabrielopeyemi <famosipe2010@gmail.com>
2024-10-12 14:06:39 +0100

    fix: update  desc

--- src/app.service.ts
+++ src/app.service.ts
@@ -2,7 +2,11 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  getHello() {
+    return {
+      name: 'COG core api',
+      date: '07 october 2014',
+      describe: 'we move'
+    };
   }
 }
