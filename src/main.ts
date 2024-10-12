2522cfe4d83e5b59e6e817d784e1c64c68fdec3a
gabrielopeyemi <famosipe2010@gmail.com>
2024-10-12 13:18:10 +0100

    feat: added port to code base

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,13 @@ import { NestFactory } from '@nestjs/core';
 import { SwaggerModule, DocumentBuilder, SwaggerCustomOptions } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import * as dotenv from 'dotenv';
+
+dotenv.config(); 
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const port = process.env.PORT || 4000;
 
   app.useGlobalPipes(
     new ValidationPipe({
@@ -31,7 +35,7 @@ async function bootstrap() {
     };
 
   SwaggerModule.setup('api-docs', app, document, customOptions);
-
-  await app.listen(3000);
+  console.log(`This application is running on: ${port}`);
+  await app.listen(port);
 }
 bootstrap();
